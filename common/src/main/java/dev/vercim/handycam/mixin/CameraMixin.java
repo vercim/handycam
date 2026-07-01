b0fc10ce8f1c55593d20761e853dc4710be5f03a
vercim <67692844+vercim@users.noreply.github.com>
2026-07-01 20:41:08 +0300

    update

--- common/src/main/java/dev/vercim/handycam/mixin/CameraMixin.java
+++ common/src/main/java/dev/vercim/handycam/mixin/CameraMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
 import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -44,16 +45,32 @@ public abstract class CameraMixin {
         
         
         CameraAccessor self = (CameraAccessor) (Object) this;
-        Quaternionf rotation = self.getRotation();
-        if (Math.abs(offset.pitch) > 1.0e-4f) rotation.rotateX( offset.pitch * Mth.DEG_TO_RAD);
-        if (Math.abs(offset.yaw)   > 1.0e-4f) rotation.rotateY(-offset.yaw   * Mth.DEG_TO_RAD);
-        if (Math.abs(offset.roll)  > 1.0e-4f) rotation.rotateZ( offset.roll  * Mth.DEG_TO_RAD);
-        if (Math.abs(offset.y)     > 1.0e-5f) self.invokeMove(0f, offset.y, 0f);
 
-        
+        // 1.20.1 Camera stores orientation differently than newer versions:
+        // refresh vanilla yaw/pitch through setRotation(), then apply custom roll
+        // and rebuild the basis vectors that Camera.move() depends on.
+        float newXRot = self.getXRot() - offset.pitch;
+        float newYRot = self.getYRot() + offset.yaw;
+        self.invokeSetRotation(newYRot, newXRot);
+
+        Quaternionf rotation = self.getRotation();
+        if (Math.abs(offset.roll) > 1.0e-4f) {
+            rotation.rotateZ(-offset.roll * Mth.DEG_TO_RAD);
+            Vector3f forwards = self.getForwards();
+            Vector3f up = self.getUp();
+            Vector3f left = self.getLeft();
+            forwards.set(0f, 0f, 1f).rotate(rotation);
+            up.set(0f, 1f, 0f).rotate(rotation);
+            left.set(1f, 0f, 0f).rotate(rotation);
+        }
+
+        if (Math.abs(offset.y) > 1.0e-5f) {
+            self.invokeMove(0f, offset.y, 0f);
+        }
+
         if (isFirstPerson) {
-            self.setXRot(self.getXRot() + offset.pitch);
-            self.setYRot(self.getYRot() + offset.yaw);
+            self.setXRot(newXRot);
+            self.setYRot(newYRot);
         }
     }
 }
